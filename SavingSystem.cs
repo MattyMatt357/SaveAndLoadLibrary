4f816bc3d805e9a24e66683cce442aff6407488d
MattyMatt357 <mc21517@my.acm.ac.uk>
2024-08-02 16:47:12 +0100

    Add project files.

---
+++ SavingSystem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace SavingAndLoadingLibrary
+{
+    public class SavingSystem: MonoBehaviour
+    {
+        public void SaveJson<T>(T savedObj, string path)
+        {
+            string filepath = Application.persistentDataPath + path;
+            string playerData = JsonUtility.ToJson(savedObj, true);
+            System.IO.File.WriteAllText(filepath, playerData);
+        }
+
+        public void LoadJson<T>(T savedObj, string path)
+        {
+            string jsonFile = Application.persistentDataPath + path;
+            string loadedObject = System.IO.File.ReadAllText(jsonFile);
+            JsonUtility.FromJsonOverwrite(loadedObject, savedObj);
+
+        }
+    }
+
+}
